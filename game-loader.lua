b92053634e06f8b6a8f30d27b3ac8d90daf4da9c
ForeverSaad <ThisIsForCh33se@gmail.com>
2022-06-05 10:09:58 -0700

    Update game-loader.lua

--- game-loader.lua
+++ game-loader.lua
@@ -1,4 +1,5 @@
 local PlaceID = game.PlaceId
 
 if PlaceID == 7560156054 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ForeverSaad/EgggHub/main/games/ClickerSimulator.lua"))();
+end
